c48fbd1803e1ba1cb5f69ebeedeb5bd0aa4591d9
CezarHoraDev <cezarino.hora@estudante.ifmt.edu.br>
2024-03-23 21:12:48 -0400

    Inicial
    inicial

---
+++ src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
@@ -0,0 +1,51 @@
+package edu.ifmt.cobrancaifmt.controller;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.ifmt.cobrancaifmt.model.StatusTitulo;
+import edu.ifmt.cobrancaifmt.model.Titulo;
+import edu.ifmt.cobrancaifmt.repository.Titulos;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	@RequestMapping("/novo")
+	public ModelAndView novo() {//metodo que reorna uma string com o nome da view
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+//		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
+		
+	}
+	@RequestMapping 
+	public ModelAndView pesquisar() {
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+	
+	@Autowired //anotação que irá injetar dependencia
+	private Titulos titulos;
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Foi salvo com sucesso!");
+		return mv;
+	}
+
+	}
